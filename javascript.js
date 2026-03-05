8db55fbf77b51ba02499386e9b69790e36fa65b3
Baby_oil <nedas.matuzas@elev.ga.lbs.se>
2026-03-05 10:23:35 +0100

    phone upgrade

--- javascript.js
+++ javascript.js
@@ -1,12 +1,15 @@
 const observer = new IntersectionObserver((entries)=>{
     entries.forEach((entry)=>{
         if(entry.isIntersecting){
-            console.log(entry.target)
             entry.target.classList.add("show")
         }else{
             entry.target.classList.remove("show")
         }
     })
-}, {})
+}, {
+    threshold: 0.2
+})
+
 const elementsToObserve = document.querySelectorAll(".produktbild, .product-button, .one, .two, .buttonOne, .buttonTwo, .texting, .texting2")
+
 elementsToObserve.forEach(el => observer.observe(el))
