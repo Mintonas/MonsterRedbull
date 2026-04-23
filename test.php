1c81efd7bc11f1b9326f926fb3c2692eeae1a8a2
Baby_oil <nedas.matuzas@elev.ga.lbs.se>
2026-04-23 15:11:28 +0200

    Create test.php

---
+++ test.php
@@ -0,0 +1,30 @@
+<?php
+
+$conn = new mysqli("localhost","Mintonas","Balionas","Diddy_Club");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $Rankings = $_POST["Rankings"]
+    $Sizes = $_POST["Sizes"]
+
+    $sql = "INSERT INTO formdata (Rankings, description) VALUES ( $Rankings, $Sizes)";
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="POST">
+    Titel: <input type="text" name="Rankings" required><br><br>
+    Beskrivning:<br>
+    <textarea name="Sizes" rows="5" cols="40"></textarea><br><br>
+    <input type="submit" value="Skicka">
+</form>
+</body>
+</html>
